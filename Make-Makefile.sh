3424731c19b1e284a49c56854a157fce4f8e9ad6
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-03-29 10:30:59 -0300

    Added functionality to insert executable name

--- Make-Makefile.sh
+++ Make-Makefile.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
+echo "Digite um nome para o executável final: "
+read nomeExecutavel
+
 touch Makefile
 
-
-echo '# Name of the project
-PROJ_NAME=printy
+cat <<EOF > Makefile
+# Name of the project
+PROJ_NAME=${nomeExecutavel}
 
 # .c files
-C_SOURCE=$(wildcard *.c)
+C_SOURCE=\$(wildcard *.c)
 
 # .h files
-H_SOURCE=$(wildcard *.h)
+H_SOURCE=\$(wildcard *.h)
 
 # Object files
-OBJ=$(C_SOURCE:.c=.o)
+OBJ=\$(C_SOURCE:.c=.o)
 
 # Compiler
 CC=gcc
 
 # Flags for compiler
-CC_FLAGS=-c         \
-         -W         \
-         -Wall      \
-         -ansi      \
-         -pedantic
+CC_FLAGS=-c -W -Wall -ansi -pedantic
 
 #
 # Compilation and linking
 #
-all: $(PROJ_NAME)
+all: \$(PROJ_NAME)
 
-$(PROJ_NAME): $(OBJ)
-    $(CC) -o $@ $^
+\$(PROJ_NAME): \$(OBJ)
+    \$(CC) -o \$@ \$^
 
 %.o: %.c %.h
-    $(CC) -o $@ $< $(CC_FLAGS)
+    \$(CC) -o \$@ \$< \$(CC_FLAGS)
 
-main.o: main.c $(H_SOURCE)
-    $(CC) -o $@ $< $(CC_FLAGS)
+main.o: main.c \$(H_SOURCE)
+    \$(CC) -o \$@ \$< \$(CC_FLAGS)
 
 clean:
-    rm -rf *.o $(PROJ_NAME) *~' > Makefile
+    rm -rf *.o \$(PROJ_NAME) *~' 
+EOF
+
+echo "Makefile criado com sucesso!"
